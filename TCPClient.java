66240af52bd1a0ac19c2e5259c4ca0721501fa79
Luis Garcia <luis@Luiss-MacBook-Pro.local>
2021-02-11 20:01:16 -0800

    final commit

--- TCPClient.java
+++ TCPClient.java
@@ -1,29 +1,43 @@
+//Luis Garcia
+//Assignment 2
+//CSCD 330
+/*
+    Problem description: The goal of this program is to implement a number to words converter utilizing
+    a TCPServer and Client.
+ */
 import java.io.*;
 import java.net.Socket;
 
-public class TCPClient {
-    public static void main(String[] args) throws Exception {
+/*
+    Much like the server we create a socket that'll connect to our designated port but no functions other than
+    reading sending and receiving input to the server. When 00 is sent out we'll close the client.
+ */
 
+public class TCPClient {
+    public static void main(String[] args){
         try (Socket clientSocket = new Socket("localhost", 6787)) {
 
             OutputStream output = clientSocket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
-            //
+
             Console console = System.console();
 
-            String ogSentence;//original sentence before server
+            String clientSentence;//original sentence before server
             String modifiedSentence;//after the server makes the changes
+
+            //Sending and receiving values.
             do{
-                ogSentence = console.readLine("Enter a number: ");
-                writer.println(ogSentence);
+                clientSentence = console.readLine("Enter a number: ");
+                writer.println(clientSentence);
+
                 InputStream input = clientSocket.getInputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
                 modifiedSentence = reader.readLine();
                 System.out.println(modifiedSentence);
-            }while(!ogSentence .equals("00"));
-            clientSocket.close();
-        }catch (NumberFormatException e){
-            System.out.println("INVALID INPUT...");
+            }while(!clientSentence .equals("00"));
+        }catch (Exception e){
+            System.out.println("error...");
         }
     }
 }
--- numWords.java
+++ numWords.java
@@ -1,15 +1,6 @@
 //Luis Garcia
-//Assignment 2
 //CSCD 330
-/*
-    Problem description: The goal of this program is to implement a number to words converter utilizing
-    a TCPServer and Client.
- */
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-public class TCPServer {
+public class numWords {
 
     //Word bank for all cases of passed in numbers.
     private String[] singleNumbers = {"","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
@@ -17,52 +8,15 @@ public class TCPServer {
     private String[] tens = {"","", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
     private String[] misc = {"","thousand ", "million ", "billion "};
 
-    /*The main function of this class is to create a server socket and be listening for incoming connections to our designated port
-      and on connection we'll be listening for client input and based on whether it's 00 or a value we can convert
-      we'll send out a string pertaining what was passed in from the client. Once the client has given us the command to
-      end the program we close the socket and the client disconnects.
-     */
-    public static void main(String[] args) throws Exception {
-        TCPServer ref = new TCPServer();//for calling non static methods.
+    public static void main (String[] args){
+        numWords ref = new numWords();
+        System.out.println("Number passed in .699" +
+                "");
+        System.out.println(ref.partitionAndOutput(".699"));
+        System.out.println(ref.isANumber("21371293872139878291371923"));
+        System.out.println(ref.isANumber("."));
 
-        try(ServerSocket welcomeSocket = new ServerSocket(6787)) {
-            System.out.println("SERVER ON....");
-
-            while(true) {
-                Socket connectionSocket = welcomeSocket.accept();
-                System.out.println("CLIENT SUCCESSFULLY CONNECTED");
-                //setting up our readers
-                InputStream input = connectionSocket.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                //setting up writers
-                OutputStream out = connectionSocket.getOutputStream();
-                PrintWriter writer = new PrintWriter(out, true);
-                //misc for catching values
-                String clientSentence = "";
-                String wordToNum;
-                //main function of the program
-                do {
-                    try {
-                        clientSentence = reader.readLine();
-                        if(!ref.isANumber(clientSentence)){
-                            writer.println("FROM SERVER: The passed in value is not a number, please try again! ");
-                        }
-                        else if(clientSentence.equals("00")){
-                            writer.println("FROM SERVER: Thank you, come again");
-                        }else {
-                            wordToNum = ref.partitionAndOutput(clientSentence) + '\n';//calling num to words convert
-                            writer.println("FROM SERVER: " + wordToNum);
-                        }
-                    }
-                    catch(java.lang.NumberFormatException e){
-                        writer.println("FROM SERVER: The number that you passed in is too big, please try again!");
-                    }
-                }while (!clientSentence.equals("00"));
-                System.out.println("CLIENT DISCONNECTED");
-                connectionSocket.close();
-            }
-        }
-    }//end of main
+    }
 
     //Breaks down our inputs and returns an output String.
     public String partitionAndOutput(String temp){
@@ -91,7 +45,7 @@ public class TCPServer {
             index++;
         }
         if(num.charAt(0) == '-'){
-            return "Negative value passed in, please try again!";
+            return "Negative value passed in error...";
         }
         //printing num-words based on cases
         //if we have no decimals then we will follow this method
@@ -143,7 +97,7 @@ public class TCPServer {
         }
         return target;
     }
-    //helper method for our driver method that parses values less than three
+    //helper method that parses values less than three
     public String valuesLessThanThree (int num){
         String target = "";
         int tempNum = num % 100;
@@ -174,8 +128,9 @@ public class TCPServer {
         try{
             Double temp = Double.parseDouble(input);
         }catch (NumberFormatException e){
+            System.out.println("The passed in value is not a number.");
             return false;
         }
         return true;
     }
-}//end of class
+}
